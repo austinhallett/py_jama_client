364f7a15bca11c0f989a2a096e25d96d6abb2471
auhallet <austin.hallett-doughty@silabs.com>
2024-01-08 16:46:34 -0500

    use test client instance fixture

--- test/test_client_init.py
+++ test/test_client_init.py
@@ -3,13 +3,6 @@ from py_jama_client.core import Core
 from py_jama_client.client import JamaClient, BaseClient
 
 
-def test_client_creation():
-    client = JamaClient(
-        core=Core(
-            host=os.getenv("HOST"),
-            credentials=(os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET")),
-            verify=False,
-            oauth=True,
-        )
-    )
+def test_client_creation(get_test_jama_client):
+    client = get_test_jama_client
     assert isinstance(client, BaseClient)

ff3f9776d1c2b6fdba299527b541895363ffacd4
auhallet <austin.hallett-doughty@silabs.com>
2024-01-08 16:39:58 -0500

    ignore async for now

--- test/test_client_init.py
+++ test/test_client_init.py
@@ -14,16 +14,3 @@ def test_client_creation():
     )
     response = client.get_baselines(project_id=82)
     assert response is not None
-
-
-async def test_async_client_creation():
-    client = JamaClient(
-        core=AsyncCore(
-            host=os.getenv("HOST"),
-            credentials=(os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET")),
-            verify=False,
-            oauth=True,
-        )
-    )
-    response = await client.get_available_endpoints()
-    assert response is not None

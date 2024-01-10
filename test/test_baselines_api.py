e1993b04276738766399b7664fd438bab547437d
auhallet <austin.hallett-doughty@silabs.com>
2024-01-10 16:48:53 -0500

    add test to demo resource embedding

--- test/test_baselines_api.py
+++ test/test_baselines_api.py
@@ -1,3 +1,11 @@
 def test_get_baselines(get_test_jama_client):
     response = get_test_jama_client.get_baselines(project_id=82)
     assert response.data is not None
+
+
+def test_get_baselines_with_link(get_test_jama_client):
+    response = get_test_jama_client.get_baselines(
+        project_id=82,
+        params={"include": ("data.project", "data.createdBy")},
+    )
+    assert len(response.linked) > 0

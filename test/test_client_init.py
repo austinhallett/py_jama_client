6c2808651e9d53cde7950ddab3ac4881fc914ab6
auhallet <austin.hallett-doughty@silabs.com>
2024-01-15 18:10:39 -0500

    update client init test

--- test/test_client_init.py
+++ test/test_client_init.py
@@ -1,6 +1,5 @@
 import os
-from py_jama_client.core import Core
-from py_jama_client.client import JamaClient, BaseClient
+from py_jama_client.client import BaseClient
 
 
 def test_client_creation(get_test_jama_client):

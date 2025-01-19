48dc2cfb4a191295c8e6033ffc9640dd17b6b56c
Tracy S. Fitch <tracy.fitch@etcconnect.com>
2025-01-19 13:13:15 -0600

    Cleanup item_types tests

--- test/test_item_types_api.py
+++ test/test_item_types_api.py
@@ -7,7 +7,7 @@ def test_get_item_types(get_test_jama_client):
     assert response.data != []
 
 
-def test_get_tag(get_test_jama_client):
+def test_get_item_type(get_test_jama_client, real_item_type):
     item_types_api = ItemTypesAPI(get_test_jama_client)
-    response = item_types_api.get_item_type(item_type_id=1)
+    response = item_types_api.get_item_type(item_type_id=real_item_type)
     assert response.data is not None

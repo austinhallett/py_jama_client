55dd49a424187d54650aed492dae1eb7f67901b9
Tracy S. Fitch <tracy.fitch@etcconnect.com>
2025-01-19 09:50:04 -0600

    Expand baselines_api tests

--- test/conftest.py
+++ test/conftest.py
@@ -3,6 +3,12 @@ import ssl
 
 import pytest
 
+from py_jama_client.apis.abstract_items_api import AbstractItemsAPI
+from py_jama_client.apis.baselines_api import BaselinesAPI
+from py_jama_client.apis.item_types_api import ItemTypesAPI
+from py_jama_client.apis.projects_api import ProjectsAPI
+from py_jama_client.apis.relationships_api import RelationshipsAPI
+from py_jama_client.apis.tags_api import TagsAPI
 from py_jama_client.client import JamaClient
 from py_jama_client.response import ClientResponse
 
@@ -136,3 +142,145 @@ def get_example_client_response():
             },
         ],
     )
+
+
+@pytest.fixture(scope="session")
+def real_abstract_item(get_test_jama_client, real_project, real_item_type):
+    if 'ABSTRACT_ITEM_ID' in os.environ:
+        return os.environ.get('ABSTRACT_ITEM_ID')
+    abstract_items_api = AbstractItemsAPI(get_test_jama_client)
+    abstract_items = abstract_items_api.get_abstract_items(
+        project=(real_project,),
+        item_type=(real_item_type,)).data
+    if abstract_items == []:
+        raise ValueError(
+            """
+                Unable to identify a viable abstract item for sample testing
+            """)    
+    else:
+        return abstract_items[0]['id']
+
+
+@pytest.fixture(scope="session")
+def real_abstract_item_version(get_test_jama_client, real_abstract_item):
+    if 'ABSTRACT_ITEM_VERSION' in os.environ:
+        return os.environ.get('ABSTRACT_ITEM_VERSION')
+    abstract_items_api = AbstractItemsAPI(get_test_jama_client)
+    abstract_item_versions = abstract_items_api.get_abstract_item_versions(
+        item_id=real_abstract_item).data
+    if abstract_item_versions == []:
+        raise ValueError(
+            """
+                Unable to identify a viable abstract item for sample
+                version testing
+            """)    
+    else:
+        return abstract_item_versions[0]['id']
+
+
+@pytest.fixture(scope="session")
+def real_baseline(get_test_jama_client, real_project):
+    if 'BASELINE_ID' in os.environ:
+        return os.environ.get('BASELINE_ID')
+    baselines_api = BaselinesAPI(get_test_jama_client)
+    baselines = baselines_api.get_baselines(project_id=real_project).data
+    if baselines == []:
+        raise ValueError(
+            """
+                Unable to identify a viable baseline for sample testing
+            """)    
+    else:
+        return baselines[0]['id']
+
+
+@pytest.fixture(scope="session")
+def real_baseline_versioned_item(get_test_jama_client, real_baseline):
+    if 'BASELINE_ID' in os.environ:
+        return os.environ.get('BASELINE_ID')
+    baselines_api = BaselinesAPI(get_test_jama_client)
+    baseline_versioned_items = baselines_api.get_baseline_versioned_items(
+        baseline_id=real_baseline).data
+    if baseline_versioned_items == []:
+        raise ValueError(
+            """
+                Unable to identify a viable baseline for sample testing
+            """)    
+    else:
+        return baseline_versioned_items[0]['id']
+
+
+@pytest.fixture(scope="session")
+def real_project(get_test_jama_client):
+    if 'PROJECT_ID' in os.environ:
+        return os.environ.get('PROJECT_ID')
+    projects_api = ProjectsAPI(get_test_jama_client)
+    projects = projects_api.get_projects().data
+    if projects == []:
+        raise ValueError(
+            """
+                Unable to identify a viable project for sample testing
+            """)    
+    # return projects[0]['id']
+    else:
+        return 350
+
+
+@pytest.fixture(scope="session")
+def real_item_type(get_test_jama_client):
+    if 'ITEM_TYPE_ID' in os.environ:
+        return os.environ.get('ITEM_TYPE_ID')
+    item_types_api = ItemTypesAPI(get_test_jama_client)
+    item_types = item_types_api.get_item_types().data
+    if item_types == []:
+        raise ValueError(
+            """
+                Unable to identify a viable item type for sample testing
+            """)    
+    else:
+        return item_types[0]['id']
+
+
+@pytest.fixture(scope="session")
+def real_relationship(get_test_jama_client, real_project):
+    if 'RELATIONSHIP_ID' in os.environ:
+        return os.environ.get('RELATIONSHIP_ID')
+    relationships_api = RelationshipsAPI(get_test_jama_client)
+    relationships = relationships_api.get_relationships(
+        project_id=real_project).data
+    if relationships == []:
+        raise ValueError(
+            """
+                Unable to identify a viable relationship for sample testing
+            """)    
+    else:
+        return relationships[0]['id']
+
+
+@pytest.fixture(scope="session")
+def real_relationship_type(get_test_jama_client):
+    if 'RELATIONSHIP_TYPE_ID' in os.environ:
+        return os.environ.get('RELATIONSHIP_TYPE_ID')
+    relationships_api = RelationshipsAPI(get_test_jama_client)
+    relationship_types = relationships_api.get_relationship_types().data
+    if relationship_types == []:
+        raise ValueError(
+            """
+                Unable to identify a viable relationship type for sample testing
+            """)    
+    else:
+        return relationship_types[0]['id']
+
+
+@pytest.fixture(scope="session")
+def real_tag(get_test_jama_client, real_project):
+    if 'TAG_ID' in os.environ:
+        return os.environ.get('TAG_ID')
+    tags_api = TagsAPI(get_test_jama_client)
+    tags = tags_api.get_tags(project_id=real_project).data
+    if tags == []:
+        raise ValueError(
+            """
+                Unable to identify a viable tag for sample testing
+            """)    
+    else:
+        return tags[0]['id']
--- test/test_baselines_api.py
+++ test/test_baselines_api.py
@@ -1,16 +1,57 @@
 from py_jama_client.apis.baselines_api import BaselinesAPI
 
 
-def test_get_baselines(get_test_jama_client):
+def test_get_baselines(get_test_jama_client, real_project):
     baselines_api = BaselinesAPI(get_test_jama_client)
-    response = baselines_api.get_baselines(project_id=82)
+    response = baselines_api.get_baselines(project_id=real_project)
     assert response.data is not None
 
 
-def test_get_baselines_with_link(get_test_jama_client):
+def test_get_baseline(get_test_jama_client, real_baseline):
+    baselines_api = BaselinesAPI(get_test_jama_client)
+    response = baselines_api.get_baseline(baseline_id=real_baseline)
+    assert response.data is not None
+
+
+def test_get_baseline_review_link(get_test_jama_client, real_baseline):
+    baselines_api = BaselinesAPI(get_test_jama_client)
+    response = baselines_api.get_baseline_review_link(
+        baseline_id=real_baseline)
+    assert response.data is not None
+
+
+def test_get_baseline_versioned_items(get_test_jama_client, real_baseline):
+    baselines_api = BaselinesAPI(get_test_jama_client)
+    response = baselines_api.get_baseline_versioned_items(
+        baseline_id=real_baseline)
+    assert response.data is not None
+
+
+def test_get_baseline_versioned_item(get_test_jama_client,
+                                     real_baseline,
+                                     real_baseline_versioned_item):
+    baselines_api = BaselinesAPI(get_test_jama_client)
+    response = baselines_api.get_baseline_versioned_item(
+        baseline_id=real_baseline,
+        item_id=real_baseline_versioned_item)
+    assert response.data is not None
+
+
+def test_get_baseline_versioned_item_relationships(
+        get_test_jama_client,
+        real_baseline,
+        real_baseline_versioned_item):
+    baselines_api = BaselinesAPI(get_test_jama_client)
+    response = baselines_api.get_baseline_versioned_item_relationships(
+        baseline_id=real_baseline,
+        item_id=real_baseline_versioned_item)
+    assert response.data is not None
+
+
+def test_get_baselines_with_link(get_test_jama_client, real_project):
     baselines_api = BaselinesAPI(get_test_jama_client)
     response = baselines_api.get_baselines(
-        project_id=82,
+        project_id=real_project,
         params={"include": ("data.project", "data.createdBy")},
     )
     assert len(response.linked) > 0

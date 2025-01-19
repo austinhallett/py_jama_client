76f16863d0c81f6f25bd18b6ed5e1bb3f24cfdb9
Tracy S. Fitch <tracy.fitch@etcconnect.com>
2025-01-19 13:13:49 -0600

    Genericize tags_api tests

--- test/test_tags_api.py
+++ test/test_tags_api.py
@@ -1,19 +1,19 @@
 from py_jama_client.apis.tags_api import TagsAPI
 
 
-def test_get_tags(get_test_jama_client):
+def test_get_tags(get_test_jama_client, real_project):
     tags_api = TagsAPI(get_test_jama_client)
-    response = tags_api.get_tags(project_id=195)
+    response = tags_api.get_tags(project_id=real_project)
     assert response.data != []
 
 
-def test_get_tag(get_test_jama_client):
+def test_get_tag(get_test_jama_client, real_tag):
     tags_api = TagsAPI(get_test_jama_client)
-    response = tags_api.get_tag(tag_id=1)
+    response = tags_api.get_tag(tag_id=real_tag)
     assert response.data is not None
 
 
-def test_get_tag_items(get_test_jama_client):
+def test_get_tag_items(get_test_jama_client, real_tag):
     tags_api = TagsAPI(get_test_jama_client)
-    response = tags_api.get_tag_items(tag_id=1)
+    response = tags_api.get_tag_items(tag_id=real_tag)
     assert response.data != []

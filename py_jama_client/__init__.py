41e71311ec43c3126e608ce5e029a39a7a90f8fb
austinhallett <austin.hallett.doughty@gmail.com>
2025-03-06 07:57:08 -0500

    ignore f403 on module root

--- py_jama_client/__init__.py
+++ py_jama_client/__init__.py
@@ -1,5 +1,5 @@
-from .apis import *
-from .client import *
-from .exceptions import *
-from .helpers import *
-from .response import *
+from .apis import * # noqa: F403
+from .client import * # noqa: F403
+from .exceptions import * # noqa: F403
+from .helpers import * # noqa: F403
+from .response import * # noqa: F403
